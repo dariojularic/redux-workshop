cfb8bb1dd2db2bad69ed02b2fe206831c9bf8fe5
Dario Jularic <dariojularic0@gmail.com>
2024-12-15 14:04:22 +0100

    Follow workshop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Modal from "./components/Modal.jsx";
+import CartContainer from "./components/CartContainer.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice.js";
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
 
-  return <h2>Redux Toolkit</h2>
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
 
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
 }
 
-export default App
+export default App;
